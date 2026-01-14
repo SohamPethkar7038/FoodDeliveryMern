73ddd4e2b6d068b5b7cc558e63fecb87d409ed69
SohamPethkar7038 <sohampethkar04@gmail.com>
2026-01-14 23:09:45 +0530

    setup backend projet

---
+++ backend/server.js
@@ -0,0 +1,25 @@
+import express from 'express'
+import dotenv from 'dotenv'
+import cors from 'cors'
+
+// load env variables
+dotenv.config()
+
+// app config
+const app=express();
+const PORT=process.env.PORT || 4000
+
+
+// middlewares
+app.use(express.json());
+app.use(cors())
+
+
+app.get('/',(req,res)=>{
+    res.send("started with backend for food delivery project");
+})
+
+
+app.listen(PORT,()=>{
+    console.log(`server started on http://localhost:${PORT}`);
+})
