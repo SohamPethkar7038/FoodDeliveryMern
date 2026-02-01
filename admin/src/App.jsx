295c61b96fc7ab21a8d7c0607df4deac9a4aeff0
SohamPethkar7038 <sohampethkar04@gmail.com>
2026-02-01 20:08:50 +0530

    Add navbar in admin

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,11 +1,31 @@
+import React from "react"
+import {Routes,Route} from 'react-router-dom'
 
+import Navbar from "./components/Navbar/Navbar"
+import Sidebar from "./components/Sidebar/Sidebar"
+import Add from "./pages/Add/Add"
+import Orders from "./pages/Orders/Orders"
+import List from "./pages/List/List"
+import { ToastContainer } from "react-toastify"
 
 function App() {
 
 
   return (
    <div>
-    started developing admin page
+    <ToastContainer position="top-right" autoClose={3000}/>
+    <Navbar/>
+    <hr/>
+
+    <div className="app-content">
+      <Sidebar/>
+      
+      <Routes>
+        <Route path='/add' element={<Add/>} />
+        <Route path='/list' element={<List/>} />
+        <Route path='/orders' element={<Orders/>} />
+      </Routes>
+    </div>
    </div>
      
   )
